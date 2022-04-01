397bba955881a12cffaeb8c09d16cfe91a271a66
Luanox <realluanox@gmail.com>
2022-04-02 00:24:41 +0900

    라우터 모듈 설정

---
+++ frontend/src/app/router.ts
@@ -0,0 +1,12 @@
+import {NgModule} from "@angular/core";
+import {RouterModule, Routes} from "@angular/router";
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+const routes: Routes = [
+];
+
+export class RoutingModule { }
