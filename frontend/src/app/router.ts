0fba4bc137e931a7af8406190b84a99340ae8a92
Luanox <realluanox@gmail.com>
2022-04-07 04:46:11 +0900

    라우터 생성

--- frontend/src/app/router.ts
+++ frontend/src/app/router.ts
@@ -1,8 +1,18 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
+import {IndexComponent} from "./subDirs/index/index.component";
+import {SearchComponent} from "./subDirs/search/search.component";
 
 
 const routes: Routes = [
+  {
+    path: '',
+    component: IndexComponent
+  },
+  {
+    path: 'search',
+    component: SearchComponent
+  },
 ];
 
 @NgModule({
@@ -10,4 +20,5 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 
-export class RoutingModule { }
+export class RoutingModule {
+}
