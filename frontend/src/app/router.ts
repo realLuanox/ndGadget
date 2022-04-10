f1a6732a49a3a1fa44c6c4da9a2838bfe641ed05
Luanox <realluanox@gmail.com>
2022-04-11 05:53:50 +0900

    라우터 변수 및 컴포넌트 목록 추가

--- frontend/src/app/router.ts
+++ frontend/src/app/router.ts
@@ -2,6 +2,10 @@ import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {IndexComponent} from "./subDirs/index/index.component";
 import {SearchComponent} from "./subDirs/search/search.component";
+import {AddAlcoholComponent} from "./subDirs/add-alcohol/add-alcohol.component";
+import {AddAlcoholPriceComponent} from "./subDirs/add-alcohol-price/add-alcohol-price.component";
+import {CalcAlcoholPriceComponent} from "./subDirs/calc-alcohol-price/calc-alcohol-price.component";
+import {SingleComponent} from "./subDirs/single/single.component";
 
 
 const routes: Routes = [
@@ -13,6 +17,22 @@ const routes: Routes = [
     path: 'search',
     component: SearchComponent
   },
+  {
+    path: 'add/alcohol',
+    component: AddAlcoholComponent
+  },
+  {
+    path: 'add/price',
+    component: AddAlcoholPriceComponent
+  },
+  {
+    path: 'calculate',
+    component: CalcAlcoholPriceComponent
+  },
+  {
+    path: 'alcohol/:uuid',
+    component: SingleComponent
+  }
 ];
 
 @NgModule({
