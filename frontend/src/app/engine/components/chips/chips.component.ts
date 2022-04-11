1a954bb96b3feb8fbdbde703a05b7cb3a223364a
Luanox <realluanox@gmail.com>
2022-04-11 12:49:11 +0900

    category 와 detailedCategory 를 쿼리해주는 Chips 컴포넌트 작성

--- frontend/src/app/engine/components/chips/chips.component.ts
+++ frontend/src/app/engine/components/chips/chips.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Alcohol} from "../../interfaces/alcohol";
 
 @Component({
   selector: 'app-chips',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chips.component.scss']
 })
 export class ChipsComponent implements OnInit {
+  @Input() alcohol: Alcohol | null = null;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
-
 }
