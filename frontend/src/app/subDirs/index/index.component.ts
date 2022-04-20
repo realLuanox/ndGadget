b6b857d4d8c36670972e012228ad3599758eff61
Luanox <realluanox@gmail.com>
2022-04-21 06:49:33 +0900

    index 컴포넌트 기초 내용 작성

--- frontend/src/app/subDirs/index/index.component.ts
+++ frontend/src/app/subDirs/index/index.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {AlcoholService} from "../../engine/services/alcohol.service";
+import {ApiService} from "../../engine/services/api.service";
+import {Alcohol} from "../../engine/interfaces/alcohol";
 
 @Component({
   selector: 'app-index',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent implements OnInit {
+  alcoholData : Alcohol[] = [];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private apiService: ApiService,
+              private alcoholService: AlcoholService) {
+    this.alcoholService.getAlcoholData().then(value =>{
+      this.alcoholData = value;
+    });
   }
 
+  ngOnInit(): void {
+    console.log(this.alcoholService.alcoholData);
+    this.apiService.get<string>('/api/hello').subscribe(value => {
+      console.log(value);
+    });
+
+    this.apiService.post<void>('/api/postTest', {
+      hello: 'World!',
+      lfasdfk: 'qewrqrewr'
+    }).subscribe(value => {});
+  }
 }
