780972b7f992b71d476d184f4e0ff1f742e6af5c
Luanox <realluanox@gmail.com>
2022-05-09 15:19:40 +0900

    StorePriceStructure 추가 fromControl 들을 formGroup으로 묶음

--- frontend/src/app/subDirs/add-alcohol-price/add-alcohol-price.component.ts
+++ frontend/src/app/subDirs/add-alcohol-price/add-alcohol-price.component.ts
@@ -1,9 +1,9 @@
 import {Component, OnInit} from '@angular/core';
-import {Alcohol, StorePrice, AlcoholStructure} from "../../engine/interfaces/alcohol";
+import {Alcohol, AlcoholStructure, StorePriceStructure} from "../../engine/interfaces/alcohol";
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {ApiService} from "../../engine/services/api.service";
 import {AlcoholService} from "../../engine/services/alcohol.service";
-import {elementAt} from "rxjs";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-add-alcohol-price',
@@ -12,30 +12,88 @@ import {elementAt} from "rxjs";
 })
 export class AddAlcoholPriceComponent implements OnInit {
   output = AlcoholStructure;
+  storePriceOutput = StorePriceStructure;
   alcohol: Alcohol[] = [];
+  uid = window.location.pathname.split('/' ).pop();
   isLinear = false;
-  amountControl = new FormControl('', [Validators.required]);
-  priceControl = new FormControl('', [Validators.required]);
+
+  storeNameExample: string[] = [
+    '석진상회',
+    '형제상회',
+    '안성상회',
+    '광천상회',
+    '하나상사',
+    '아리수상사',
+    '달리',
+    '와인25',
+    '데일리샷'
+  ];
 
 
-  firstFormGroup: FormGroup = this._formBuilder.group({
-    firstCtrl: ['', Validators.required],
+  firstFormGroup: FormGroup = this.formBuilder.group({
+    'storeNameControl' : new FormControl('', [Validators.required]),
+    'buyDateControl' : new FormControl ('', [Validators.required])
   });
-  secondFormGroup = this._formBuilder.group({
-    secondCtrl: ['', Validators.required],
+
+  secondFormGroup = this.formBuilder.group({
+    'priceControl' : new FormControl('', [Validators.required]),
+    'amountControl' : new FormControl('', [Validators.required]),
+    'buyMethodControl' : new FormControl('', [Validators.required])
   });
 
-  constructor(private _formBuilder: FormBuilder,
+  thirdFormGroup = this.formBuilder.group( {
+    'etcControl' : new FormControl('', [Validators.required])
+  })
+
+  constructor(private formBuilder: FormBuilder,
               private apiService: ApiService,
-              private alcoholService: AlcoholService) {
+              private alcoholService: AlcoholService,
+              private router: Router,) {
+    if(history.state.hasOwnProperty('name'))  {
+      this.output = history.state as Alcohol;
+      this.alcohol[0] = this.output;
+      document.title = this.output.name;
+    } else {
+      this.apiService.getAsync<Alcohol>(`/api/alcohol/${this.uid}`).then(value => {
+        this.output = value;
+        console.log(this.output);
+        this.alcohol[0] = this.output;
+        document.title = this.output.name;
+      });
+    }
   }
 
   ngOnInit() {
+  }
+
+  onUpdateFirstGroup() {
+    this.output = this.alcohol[0];
+    this.storePriceOutput.storeName = this.firstFormGroup.get('storeNameControl')!.value.trim();
+    this.storePriceOutput.buyDate = this.firstFormGroup.get('buyDateControl')!.value;
+    console.log(this.firstFormGroup.get('buyDateControl')!.value);
+  }
+
+  onUpdateSecondGroup() {
+    this.storePriceOutput.price = this.secondFormGroup.get('priceControl')!.value;
+    this.storePriceOutput.amount = this.secondFormGroup.get('amountControl')!.value;
+    this.storePriceOutput.priceType = this.secondFormGroup.get('buyMethodControl')!.value;
+  }
+
+  public sortByDate(): void {
+    console.log(this.thirdFormGroup.get('etcControl')!.value)
+    this.storePriceOutput.etc = this.thirdFormGroup.get('etcControl')!.value;
+    this.output.storePrices.push(this.storePriceOutput);
+    console.log(this.output);
   }
 
   search(keyword: string): void {
     this.alcohol = this.alcoholService.alcoholData.filter(value => value.englishName === keyword);
   }
 
-
+  submit(): void {
+    this.apiService.postAsync('/api/edit/alcohol', this.output).then(() => {
+      this.router.navigate(['/']).then();
+    }).catch(() => {
+    });
+  }
 }
