03c413b16b93b3b92fe227e704615a49c70fa271
Luanox <realluanox@gmail.com>
2022-05-12 18:54:08 +0900

    addAlcoholComponent 의 formControl들을 formGroup으로 묶고, koreanName과 englishName 필드 배열을 동시에 가져와서 만약 이름 입력란이 해당 배열에 일치하는 문자열을 반환할 경우 MatSnackBar를 띄우도록하는 기능을 구현함

--- frontend/src/app/subDirs/add-alcohol/add-alcohol.component.ts
+++ frontend/src/app/subDirs/add-alcohol/add-alcohol.component.ts
@@ -17,24 +17,18 @@ import {MatSnackBar} from "@angular/material/snack-bar";
 export class AddAlcoholComponent implements OnInit {
   output = AlcoholStructure;
   alcohol: Alcohol | null = null;
-  nameControl = new FormControl('', [Validators.required]);
   nameFilter: Observable<string[]>;
-  englishNameControl = new FormControl('', [Validators.required]);
-  imageUrlControl = new FormControl('', [Validators.required]);
-  degreeControl = new FormControl('',[Validators.required]);
-  descriptionControl = new FormControl('', [Validators.required]);
-  categoryControl = new FormControl('', [Validators.required]);
+  englishNameFilter: Observable<string[]>;
   categories = [] as string[];
   categoryFilter: Observable<string[]>
-  detailedCategoryControl = new FormControl('', [Validators.required]);
   detailedCategoryFilter: Observable<string[]>
   detailedCategories = [] as string[];
-  searched = [] as string[];
-
-
-  mustFillGroup: FormGroup = this.formBuilder.group({});
-  ageControl = new FormControl();
-  imageControl = new FormControl();
+  searchedName = [] as string[];
+  searchedEnglishName = [] as string[];
+  nameSet: {
+    korean: string;
+    english: string;
+  }[] = [];
 
   alcoholCategory: string[] = [
     '위스키',
@@ -49,70 +43,129 @@ export class AddAlcoholComponent implements OnInit {
     '기타',
   ];
 
-  isLinear = false;
   firstFormGroup: FormGroup = this.formBuilder.group({
+    'nameControl': new FormControl('', [Validators.required]),
+    'englishNameControl': new FormControl('', [Validators.required])
   });
-  secondFormGroup = this.formBuilder.group({
+
+  secondFormGroup: FormGroup = this.formBuilder.group({
+    'ageControl': new FormControl(0),
+    'degreeControl': new FormControl(0, [Validators.required]),
+    'categoryControl': new FormControl('', [Validators.required]),
+    'detailedCategoryControl': new FormControl('', [Validators.required])
   });
+
+  thirdFormGroup: FormGroup = this.formBuilder.group({
+    'imageUrlControl': new FormControl('', [Validators.required]),
+    'descriptionControl': new FormControl('', [Validators.required])
+  })
 
   constructor(
     private alcoholService: AlcoholService,
     private formBuilder: FormBuilder,
     private apiService: ApiService,
     private router: Router,
-    ) {
-    if(this.alcoholService.Alcohol) {
-      this.searched = this.alcoholService.Alcohol;
+    private snackbar: MatSnackBar
+  ) {
+    this.alcoholService.getAlcoholName().then(v => {
+      this.nameSet = v;
+    });
+    if (this.alcoholService.alcoholName) {
+      this.searchedName = this.alcoholService.alcoholName;
     } else {
-      this.alcoholService.getAlcohol().then(value => {
-        this.searched = value;
+      this.alcoholService.getAlcoholName().then(value => {
+        this.searchedName = value.map(v => v.korean);
+        this.searchedEnglishName = value.map(v => v.english)
       })
     }
-    if(this.alcoholService.categories) {
+    if (this.alcoholService.alcoholEnglishName) {
+      this.searchedEnglishName = this.alcoholService.alcoholEnglishName;
+    } else {
+
+    }
+    if (this.alcoholService.categories) {
       this.categories = this.alcoholService.categories;
     } else {
       this.alcoholService.getCategories().then(value => {
         this.categories = value;
       });
     }
-    if(this.alcoholService.detailedCategories) {
+    if (this.alcoholService.detailedCategories) {
       this.categories = this.alcoholService.detailedCategories;
     } else {
       this.alcoholService.getDetailedCategories().then(value => {
         this.categories = value;
       })
     }
-    // this.descriptionControl.valueChanges.subscribe()
+    this.thirdFormGroup.valueChanges.subscribe(value => {
+      this.output.imageUrl = value.imageUrlControl;
+      this.output.description = value.descriptionControl;
+    })
+    this.englishNameFilter = this.firstFormGroup.get('englishNameControl')!.valueChanges.pipe(startWith(''), map(value => this._filter_english_name(value)));
+    this.nameFilter = this.firstFormGroup.get('nameControl')!.valueChanges.pipe(startWith(''), map(value => this._filter_name(value)));
+    this.categoryFilter = this.secondFormGroup.get('categoryControl')!.valueChanges.pipe(startWith(''), map(value => this._filter_category(value)));
+    this.detailedCategoryFilter = this.secondFormGroup.get('detailedCategoryControl')!.valueChanges.pipe(startWith(''), map(value => this._filter_detailed_category(value)));
 
-    this.nameFilter = this.nameControl.valueChanges.pipe(startWith(''), map(value => this._filter_name(value)));
-    this.categoryFilter = this.categoryControl.valueChanges.pipe(startWith(''), map(value => this._filter_category(value)));
-    this.detailedCategoryFilter = this.detailedCategoryControl.valueChanges.pipe(startWith(''), map(value => this._filter_detailed_category(value)));
+    if(window.location.pathname.split('/').includes('edit')) {
+      const uid = window.location.pathname.split('/').pop();
+      this.apiService.getAsync<Alcohol>(`/api/alcohol/${uid}`).then(value => {
+        this.output = value;
+        this.firstFormGroup.controls['nameControl'].setValue(this.output.name);
+        this.firstFormGroup.controls['englishNameControl'].setValue(this.output.englishName);
+        this.secondFormGroup.controls['ageControl'].setValue(this.output.age);
+        this.secondFormGroup.controls['degreeControl'].setValue(this.output.degree);
+        this.secondFormGroup.controls['categoryControl'].setValue(this.output.category[0]);
+        this.secondFormGroup.controls['detailedCategoryControl'].setValue(this.output.category[1]);
+        this.thirdFormGroup.controls['imageUrlControl'].setValue(this.output.imageUrl);
+        this.thirdFormGroup.controls['descriptionControl'].setValue(this.output.description);
+      })
+    }
   }
 
   ngOnInit(): void {
   }
 
   onUpdateFirstGroup(event: Event) {
-    this.output.name = this.nameControl.value.trim();
-    this.output.englishName = this.englishNameControl.value.trim();
-    console.log(this.alcoholService.getAlcohol())
+    this.output.name = this.firstFormGroup.get('nameControl')!.value.trim();
+    this.output.englishName = this.firstFormGroup.get('englishNameControl')!.value.trim();
   }
 
   onUpdateSecondGroup(event: Event) {
-    this.output.category[0] = this.categoryControl.value.trim();
-    this.output.category[1] = this.detailedCategoryControl.value.trim();
-    this.output.age = this.ageControl.value.trim();
-    this.output.degree = this.degreeControl.value.trim();
-    this.output.imageUrl = this.imageUrlControl.value.trim();
+    this.output.category[0] = this.secondFormGroup.get('categoryControl')!.value.trim();
+    this.output.category[1] = this.secondFormGroup.get('detailedCategoryControl')!.value.trim();
+    this.output.age = this.secondFormGroup.get('ageControl')!.value;
+    this.output.degree = this.secondFormGroup.get('degreeControl')!.value;
   }
 
-  onUpdateLastGroup(event: Event) {
-    this.output.description = this.descriptionControl.value.trim();
+  onUpdateThirdGroup(event: Event) {
+    this.output.description = this.thirdFormGroup.get('descriptionControl')!.value.trim();
+    this.output.imageUrl = this.thirdFormGroup.get('imageUrlControl')!.value.trim();
+  }
+
+  selectKoreanName(value: string) {
+    const result = this.nameSet.find(v => v.korean === value);
+    if (result) {
+      this.firstFormGroup.controls['englishNameControl'].setValue(result.english);
+      this.snackbar.open('alcohol already is in', '이동하기')
+    }
+  }
+
+  selectEnglishName(value: string) {
+    const result = this.nameSet.find(v => v.english === value);
+    if (result) {
+      this.firstFormGroup.controls['nameControl'].setValue(result.korean);
+      this.snackbar.open('alcohol already is in', '이동하기')
+    }
   }
 
   _filter_name(value: string): string[] {
     const filterValue = value.toLowerCase();
-    return this.searched.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
+    return this.searchedName.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
+  }
+
+  _filter_english_name(value: string): string[] {
+    const filterValue = value.toLowerCase();
+    return this.searchedEnglishName.filter(option => option.indexOf(filterValue) === 0)
   }
 
   _filter_category(value: string): string[] {
@@ -125,29 +178,11 @@ export class AddAlcoholComponent implements OnInit {
     return this.detailedCategories.filter(option => option.toLowerCase().indexOf(filterValue) === 0);
   }
 
-  searchAlcohol(value: string): void {
-    this.apiService.post<Alcohol[]>('/api/AlcList.json', {name: value}).subscribe(v => {
-      this.searched = v.map(k => k.name);
-    });
-  }
-
-  searchAlcoholCategory(value: Alcohol): void {
-    this.apiService.get<Alcohol[]>('/api/AlcList.json').subscribe(v => {
-    })
-  }
-
-  addAlcohol(event:KeyboardEvent): void {
+  addAlcohol(event: KeyboardEvent): void {
     event.preventDefault();
   }
 
   submit(): void {
-    // this.output 이 alcoholStructure 필드와 같은 경우 동작하지 않는 익명 함수를 작성할 수 있을까?
-    this.output.name = this.nameControl.value.trim();
-    this.output.englishName = this.englishNameControl.value.trim();
-    this.output.degree = this.degreeControl.value;
-    this.output.description = this.descriptionControl.value;
-    this.output.category[0] = this.categoryControl.value.trim();
-    this.output.category[1] = this.detailedCategoryControl.value.trim();
     if (window.location.pathname.split('/').includes('edit')) {
       this.apiService.postAsync<void>('/api/edit/alcohol', this.output).then(() => {
         this.router.navigate(['/']).then();
@@ -160,4 +195,6 @@ export class AddAlcoholComponent implements OnInit {
       });
     }
   }
+
+
 }
